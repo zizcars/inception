55c230d0c5ab436fc5ba8406a80f1f17b6945e19
achakkaf <zizcarschak1@gmail.com>
2025-01-18 16:13:39 +0100

    installing docker

--- run.sh
+++ run.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-docker rmi -f $(docker images -aq)
-docker rm -f $(docker ps -aq)
+# docker rmi -f $(docker images -aq)
+# docker rm -f $(docker ps -aq)
 # docker build -t my-nginx . --no-cache
 # docker run -d -p 5000:80 my-nginx:latest
+
+# =============== Intalling Docker =================
+
+# sudo apt update && sudo apt upgrade -y
+# sudo apt install docker.io -y
+# sudo systemctl start docker
+# sudo systemctl enable docker
+# sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
+
+# ==================================================
+
