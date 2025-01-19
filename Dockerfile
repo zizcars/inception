c21259eae404e5b6c05b2c0d367001447a1a7aed
achakkaf <zizcarschak1@gmail.com>
2025-01-19 11:29:49 +0100

    directory structure is added

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM debian:latest
-
-RUN apt update && apt install -y nginx
-
-# EXPOSE 5000:80
-
-CMD ["nginx", "-g", "daemon off;"]
--- run.sh
+++ run.sh
@@ -14,5 +14,10 @@
 # sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 # sudo chmod +x /usr/local/bin/docker-compose
 
-# ==================================================
+# ============== directory structure ================
 
+# mkdir -p secrets/ srcs/requirements/{bonus,mariadb,nginx,tools,wordpress}/{conf,tools} 
+# touch Makefile secrets/{credentials.txt,db_password.txt,db_root_password.txt} srcs/{docker-compose.yml,.env}
+# touch srcs/requirements/{mariadb,nginx,wordpress}/{Dockerfile,.dockerignore}
+
+# ============== ================ ================
