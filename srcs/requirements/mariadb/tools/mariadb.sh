e7a5a561f5bd55b051e63cd0ac2e367b5a5c6acf
achakkaf <zizcarschak1@gmail.com>
2025-02-10 13:53:07 +0100

    mariadb done

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-service mysql start
+mysqld_safe --skip-networking &
+sleep 5
 
+mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
+
+mysqladmin -uroot -p${MYSQL_ROOT_PASSWORD} shutdown
+
+exec mysqld_safe
